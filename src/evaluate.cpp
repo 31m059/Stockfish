a6f91915ebe3d9880e4df2481cd364d69b702d2f
31m059 <37052095+31m059@users.noreply.github.com>
2018-04-06 23:17:07 -0400

    Extra overload for queen.

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -172,6 +172,7 @@ namespace {
   constexpr Score LongDiagonalBishop = S( 22,  0);
   constexpr Score MinorBehindPawn    = S( 16,  0);
   constexpr Score Overload           = S( 10,  5);
+  constexpr Score OverloadedQueen    = S( 10,  0);
   constexpr Score PawnlessFlank      = S( 20, 80);
   constexpr Score RookOnPawn         = S(  8, 24);
   constexpr Score SliderOnQueen      = S( 42, 21);
@@ -614,6 +615,10 @@ namespace {
        & attackedBy[Us][ALL_PIECES]   & ~attackedBy2[Us]
        & attackedBy[Them][ALL_PIECES] & ~attackedBy2[Them];
     score += Overload * popcount(b);
+
+    // Further bonus for an overloaded queen
+    b &= attackedBy[Them][QUEEN];
+    score += OverloadedQueen * popcount(b);
 
     if (T)
         Trace::add(THREAT, Us, score);
