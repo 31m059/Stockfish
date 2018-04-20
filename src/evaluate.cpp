cc2edadea9e67e13decaecb14d4242c62deaf536
31m059 <37052095+31m059@users.noreply.github.com>
2018-04-20 15:16:01 -0400

    Only -10%.

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -814,9 +814,8 @@ namespace {
             else
                 sf = 46;
 
-            // If the strong side has a passed pawn whose promotion square the weak
-            // side's bishop cannot defend, decrease the scale factor by 10%;
-            // otherwise, decrease by 10%.
+            // If the strong side has no passed pawn whose promotion square the weak
+            // side's bishop cannot defend, decrease the scale factor by 10%.
             Bitboard passedPawns = pe->passedPawns[strongSide];
             Bitboard promotionRank = strongSide == WHITE ? Rank8BB : Rank1BB;
             Bitboard weakBishopSquares = pos.pieces(strongSide, BISHOP) & DarkSquares ? ~DarkSquares : DarkSquares;
@@ -828,7 +827,7 @@ namespace {
                     break;
                 }
             }
-            sf = strongPassedPawn ? 11 * sf / 10 : 9 * sf / 10;
+            sf = strongPassedPawn ? sf : 9 * sf / 10;
         }
         // Endings where weaker side can place his king in front of the enemy's
         // pawns are drawish.
