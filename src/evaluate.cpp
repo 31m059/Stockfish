9f8c9b131deaea1f58d9e82195d33ba8c7035f83
31m059 <37052095+31m059@users.noreply.github.com>
2018-04-20 15:11:48 -0400

    +10% sf if a passer's promotion square cannot be defended.

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -813,6 +813,21 @@ namespace {
             // a bit drawish, but not as drawish as with only the two bishops.
             else
                 sf = 46;
+
+            // If the strong side has a passed pawn whose promotion square the weak
+            // side's bishop cannot defend, increase the scale factor by 10%.
+            Bitboard passedPawns = pe->passedPawns[strongSide];
+            Bitboard promotionRank = strongSide == WHITE ? Rank8BB : Rank1BB;
+            Bitboard weakBishopSquares = pos.pieces(strongSide, BISHOP) & DarkSquares ? ~DarkSquares : DarkSquares;
+            bool strongPassedPawn = false;
+            while (passedPawns) {
+                if (file_bb(pop_lsb(&passedPawns)) & promotionRank & ~weakBishopSquares)
+                {
+                    strongPassedPawn = true;
+                    break;
+                }
+            }
+            sf = strongPassedPawn ? 11 * sf * 10 : sf;
         }
         // Endings where weaker side can place his king in front of the enemy's
         // pawns are drawish.
