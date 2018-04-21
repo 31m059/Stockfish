d56defe006cdff7271d4cf88f73cbfb173dcf3c5
31m059 <37052095+31m059@users.noreply.github.com>
2018-04-20 22:51:31 -0400

    Take 1, but no + to sf if weak side has passers.

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -811,21 +811,26 @@ namespace {
 
             // Endgame with opposite-colored bishops, but also other pieces. Still
             // a bit drawish, but not as drawish as with only the two bishops.
-            // The endgame is also less drawish if the strong side has passers
-            // which promote on squares the weak side's bishop cannot defend.
             else
-            {
                 sf = 46;
+
+            // If the strong side has a passed pawn whose promotion square the weak
+            // side's bishop cannot defend, increase the scale factor by 10%.
+            if (!pe->passedPawns[weakSide])
+            {
                 Bitboard passedPawns = pe->passedPawns[strongSide];
                 Bitboard promotionRank = strongSide == WHITE ? Rank8BB : Rank1BB;
                 Bitboard weakBishopSquares = pos.pieces(strongSide, BISHOP) & DarkSquares ? ~DarkSquares : DarkSquares;
+                bool strongPassedPawn = false;
                 while (passedPawns) {
                     if (file_bb(pop_lsb(&passedPawns)) & promotionRank & ~weakBishopSquares)
-                        sf += 4;
+                    {
+                        strongPassedPawn = true;
+                        break;
+                    }
                 }
-                sf = std::min(sf, 64);
+                sf = strongPassedPawn ? 11 * sf / 10 : sf;
             }
-
         }
         // Endings where weaker side can place his king in front of the enemy's
         // pawns are drawish.
