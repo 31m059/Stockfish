be481b425ec24adf92a8b18a3096a1128824c1c2
31m059 <37052095+31m059@users.noreply.github.com>
2018-05-06 04:15:07 -0400

    Greater effect.

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -784,11 +784,11 @@ namespace {
     // Compute the initiative bonus for the attacking side
     int complexity =   8 * outflanking
                     +  8 * pe->pawn_asymmetry()
-                    + 12 * (Overloading[WHITE] + Overloading[BLACK])
                     + 12 * pos.count<PAWN>()
                     + 16 * pawnsOnBothFlanks
+                    + 16 * (Overloading[WHITE] + Overloading[BLACK])
                     + 48 * !pos.non_pawn_material()
-                    -142 ;
+                    -143 ;
 
     // Now apply the bonus: note that we find the attacking side by extracting
     // the sign of the endgame value, and that we carefully cap the bonus so
