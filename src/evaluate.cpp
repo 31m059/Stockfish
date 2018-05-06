451fddc281e7ddc54c517010baa9538ee6af2cd3
31m059 <37052095+31m059@users.noreply.github.com>
2018-05-06 04:50:07 -0400

    Overloading of the defender only.

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -786,9 +786,9 @@ namespace {
                     +  8 * pe->pawn_asymmetry()
                     + 12 * pos.count<PAWN>()
                     + 16 * pawnsOnBothFlanks
-                    + 24 * (Overloading[WHITE] + Overloading[BLACK])
+                    + 24 * (eg > 0 ? Overloading[BLACK] : Overloading[WHITE])
                     + 48 * !pos.non_pawn_material()
-                    -147 ;
+                    -142 ;
 
     // Now apply the bonus: note that we find the attacking side by extracting
     // the sign of the endgame value, and that we carefully cap the bonus so
