4a769ac33b6cba393ec1a13cae67743b8b7d88fb
31m059 <37052095+31m059@users.noreply.github.com>
2018-05-07 11:26:35 -0400

    8 * Overloading[Defender].

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -784,7 +784,7 @@ namespace {
     // Compute the initiative bonus for the attacking side
     int complexity =   8 * outflanking
                     +  8 * pe->pawn_asymmetry()
-                    + 11 * (Overloading[WHITE] + Overloading[BLACK]) 
+                    +  8 * (eg > 0 ? Overloading[BLACK] : Overloading[WHITE]) 
                     + 12 * pos.count<PAWN>()
                     + 16 * pawnsOnBothFlanks
                     + 48 * !pos.non_pawn_material()
