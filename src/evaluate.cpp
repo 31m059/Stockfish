cb58992ae2631e4db6ddada4fe03087e6f7372bd
31m059 <37052095+31m059@users.noreply.github.com>
2018-05-09 14:52:58 -0400

    Take 2

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -616,11 +616,14 @@ namespace {
     // Bonus for overload
     // Non-pawn enemies attacked and defended exactly once)
     b  =  nonPawnEnemies
-        & attackedBy[Us][ALL_PIECES] & ~attackedBy2[Us];
+        & attackedBy[Us][ALL_PIECES]   & ~attackedBy2[Us]
+        & attackedBy[Them][ALL_PIECES] & ~attackedBy2[Them];
     // Threats to promote, if left undefended
     b |=  shift<Us == WHITE ? SOUTH : NORTH>(pos.pieces(Us, PAWN))
-        & (Us == WHITE ? RANK_8 : RANK_1);
-    score += Overload * popcount(b & attackedBy[Them][ALL_PIECES] & ~attackedBy2[Them]);
+        & (Us == WHITE ? RANK_8 : RANK_1)
+        & pos.pieces(Them)
+        & ~attackedBy[ALL_PIECES][Them];
+    score += Overload * popcount(b);
 
     if (T)
         Trace::add(THREAT, Us, score);
