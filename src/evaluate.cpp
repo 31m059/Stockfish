5cac75398501bf6a816843aec59ae742a3a84e48
31m059 <37052095+31m059@users.noreply.github.com>
2018-05-13 21:45:43 -0400

    Also if opponent has Q.

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -505,9 +505,9 @@ namespace {
     // King tropism, to anticipate slow motion attacks on our king
     score -= CloseEnemies * (popcount(b1) + popcount(b2));
 
-    // Penalty for open files against our king's surroundings, if the enemy has a rook
+    // Penalty for open files against our king's surroundings, if the enemy has a rook or queen
     Square our_ksq = (rank_of(ksq) == RANK_1 ? ksq + NORTH : ksq);
-    if (    pos.pieces(Them, ROOK)
+    if (    pos.pieces(Them, ROOK, QUEEN)
         && (passed_pawn_mask(BLACK, our_ksq) & pe->semiopenFiles[Us] & pe->semiopenFiles[Them]))
         score -= KingOpenFile;
 
