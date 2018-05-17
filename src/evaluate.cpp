a036f9a3c9a5377a38e3455e0e0245d0a63b2697
31m059 <37052095+31m059@users.noreply.github.com>
2018-05-16 23:51:16 -0400

    Extra bonus for overload pins.

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -172,6 +172,7 @@ namespace {
   constexpr Score LongDiagonalBishop = S( 22,  0);
   constexpr Score MinorBehindPawn    = S( 16,  0);
   constexpr Score Overload           = S( 10,  5);
+  constexpr Score OverloadedPin      = S( 10,  5);
   constexpr Score PawnlessFlank      = S( 20, 80);
   constexpr Score RookOnPawn         = S(  8, 24);
   constexpr Score SliderOnQueen      = S( 42, 21);
@@ -569,6 +570,7 @@ namespace {
            & attackedBy[Us][ALL_PIECES]   & ~attackedBy2[Us]
            & attackedBy[Them][ALL_PIECES] & ~attackedBy2[Them];
         score += Overload * popcount(b);
+        score += OverloadedPin * popcount(b & pos.blockers_for_king(Them));
     }
 
     // Bonus for enemy unopposed weak pawns
