cd278ead85e72993a179125a6d7b17af9a976b3f
31m059 <37052095+31m059@users.noreply.github.com>
2018-05-24 02:20:48 -0400

    Half if no attacked enemies/enemy kingRing. Bench: 5136790

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -339,14 +339,14 @@ namespace {
             bb = OutpostRanks & ~pe->pawn_attacks_span(Them);
             if (bb & s)
             {
-                Bitboard attacks = b & (pos.pieces() ^ pos.pieces(KING, PAWN));
+                Bitboard attacks = b & (pos.pieces(Them) | kingRing[Them]);
                 bool notAttacking = Pt == KNIGHT && !attacks;
                 score += Outpost[Pt == BISHOP][bool(attackedBy[Us][PAWN] & s)] * 2 / (1 + notAttacking);
             }
 
             else if (bb &= b & ~pos.pieces(Us))
             {
-                Bitboard attacks = pos.attacks_from<Pt>(lsb(bb)) & (pos.pieces() ^ pos.pieces(KING, PAWN));
+                Bitboard attacks = pos.attacks_from<Pt>(lsb(bb)) & (pos.pieces(Them) | kingRing[Them]);
                 bool notAttacking = Pt == KNIGHT && !attacks;
                 score += Outpost[Pt == BISHOP][bool(attackedBy[Us][PAWN] & bb)] / (1 + notAttacking);
             }
