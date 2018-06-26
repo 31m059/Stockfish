0700a86a93c13f5aefa5e4674ca5ab8901d42bcb
31m059 <37052095+31m059@users.noreply.github.com>
2018-06-26 02:34:14 -0400

    S(20, 20). Bench: 4638745

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -174,7 +174,7 @@ namespace {
   constexpr Score Overload           = S( 10,  5);
   constexpr Score PawnlessFlank      = S( 20, 80);
   constexpr Score RookOnPawn         = S(  8, 24);
-  constexpr Score SkewerThreat       = S(  0, 10);
+  constexpr Score SkewerThreat       = S( 20, 20);
   constexpr Score SliderOnQueen      = S( 42, 21);
   constexpr Score ThreatByPawnPush   = S( 49, 30);
   constexpr Score ThreatByRank       = S( 16,  3);
