25f56e8a66eaf220cd9a29f25a7eb8e6886f6089
31m059 <37052095+31m059@users.noreply.github.com>
2018-06-29 02:20:20 -0400

    Compensate: 19% cut to WeakQueen. Bench: 4771700

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -179,7 +179,7 @@ namespace {
   constexpr Score ThreatByRank       = S( 16,  3);
   constexpr Score ThreatBySafePawn   = S(165,133);
   constexpr Score TrappedRook        = S( 92,  0);
-  constexpr Score WeakQueen          = S( 50, 10);
+  constexpr Score WeakQueen          = S( 40,  8);
   constexpr Score WeakUnopposedPawn  = S(  5, 26);
 
 #undef S
