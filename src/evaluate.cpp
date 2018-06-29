f2a016523eeb57de92b73d99264357db486b344c
31m059 <37052095+31m059@users.noreply.github.com>
2018-06-29 02:24:56 -0400

    Sanity check: increase WeakQueen. Bench: 4805023

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -179,7 +179,7 @@ namespace {
   constexpr Score ThreatByRank       = S( 16,  3);
   constexpr Score ThreatBySafePawn   = S(165,133);
   constexpr Score TrappedRook        = S( 92,  0);
-  constexpr Score WeakQueen          = S( 40,  8);
+  constexpr Score WeakQueen          = S( 60, 12);
   constexpr Score WeakUnopposedPawn  = S(  5, 26);
 
 #undef S
