31ee8aaf54ae2d01a0b6b3fa313a82446c829155
31m059 <37052095+31m059@users.noreply.github.com>
2018-07-15 15:44:15 -0400

    Top quartile, +5%. Bench: 4579274

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -477,8 +477,8 @@ namespace {
                      -   6 * mg_value(score) / 8
                      -   2 ;
 
-        if (kingDanger > 1500 && pos.pieces(Us, QUEEN) && !pos.pieces(Them, QUEEN))
-           kingDanger += kingDanger / 10;
+        if (kingDanger > 1000 && pos.pieces(Us, QUEEN) && !pos.pieces(Them, QUEEN))
+           kingDanger += kingDanger / 20;
 
         // Transform the kingDanger units into a Score, and subtract it from the evaluation
         if (kingDanger > 0)
