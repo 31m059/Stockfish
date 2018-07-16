f2606fc3aeecf2dc2e54979117cb4cc2e7612b91
31m059 <37052095+31m059@users.noreply.github.com>
2018-07-16 00:51:51 -0400

    KD > 800. Bench: 5008212

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -477,7 +477,7 @@ namespace {
                      -   6 * mg_value(score) / 8
                      -   2 ;
 
-        if (kingDanger > 900 && pos.pieces(Us, QUEEN) && !pos.pieces(Them, QUEEN))
+        if (kingDanger > 800 && pos.pieces(Us, QUEEN) && !pos.pieces(Them, QUEEN))
            kingDanger += kingDanger / 10;
 
         // Transform the kingDanger units into a Score, and subtract it from the evaluation
