7f3dae5edab3d5909c338dfdea8653cf4a5345cb
31m059 <37052095+31m059@users.noreply.github.com>
2018-07-26 23:19:49 -0400

    Normal effect, minors only. Bench: 4892008

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -538,10 +538,10 @@ namespace {
         {
             Square s = pop_lsb(&b);
             score += ThreatByMinor[type_of(pos.piece_on(s))];
-            if (type_of(pos.piece_on(s)) != PAWN || shift<Up>(pos.pieces(Us)) & s)
+            if (type_of(pos.piece_on(s)) != PAWN)
                 score += ThreatByRank * (int)relative_rank(Them, s);
 
-            else if (pos.blockers_for_king(Them) & s)
+            else if ((pos.blockers_for_king(Them) | shift<Up>(pos.pieces(Us))) & s)
                 score += ThreatByRank * (int)relative_rank(Them, s) / 2;
         }
 
