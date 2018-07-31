1499ecf20393f51e91979e300a7648ec2be4b7da
31m059 <37052095+31m059@users.noreply.github.com>
2018-07-31 01:59:39 -0400

    Real threats only. Bench: 4689034

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -373,7 +373,7 @@ namespace {
         {
             // Bonus for aligning rook with enemy pawns on the same rank/file
             if (relative_rank(Us, s) >= RANK_5)
-                score += ThreatByRook[PAWN] * popcount(pos.pieces(Them, PAWN) & PseudoAttacks[ROOK][s]);
+                score += ThreatByRook[PAWN] * popcount(pos.pieces(Them, PAWN) & b);
 
             // Bonus for rook on an open or semi-open file
             if (pe->semiopen_file(Us, file_of(s)))
