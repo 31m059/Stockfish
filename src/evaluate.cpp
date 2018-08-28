12bbc64fec6f2b0933c74f5a7d6ff8a9f25e32ff
31m059 <37052095+31m059@users.noreply.github.com>
2018-08-28 02:15:16 -0400

    30. Bench: 4112628

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -482,8 +482,8 @@ namespace {
                      + 185 * popcount(kingRing[Us] & weak)
                      + 129 * popcount(pos.blockers_for_king(Us) | unsafeChecks)
                      +   4 * tropism
-                     +  15 * enemiesOnFile * enemiesOnFile
-                     +  15 * enemiesOnRank * enemiesOnRank
+                     +  30 * enemiesOnFile * enemiesOnFile
+                     +  30 * enemiesOnRank * enemiesOnRank
                      - 873 * !pos.count<QUEEN>(Them)
                      -   6 * mg_value(score) / 8
                      -   30;
