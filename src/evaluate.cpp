104f7f28bdff878a533e5484dcbabf1e4158968c
31m059 <37052095+31m059@users.noreply.github.com>
2018-08-31 22:10:58 -0400

    Half effect. Bench: 4324040

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -657,7 +657,7 @@ namespace {
             // If blockSq is not the queening square then consider also a second push
             bool isolated = !(pos.pieces(Us, PAWN) & adjacent_files_bb(file_of(s)));
             if (r != RANK_7)
-                bonus -= make_score(0, king_proximity(Us, blockSq + Up) * w * (1 + 2*isolated) / 2);
+                bonus -= make_score(0, king_proximity(Us, blockSq + Up) * w * (3 + 2*isolated) / 4);
 
             // If the pawn is free to advance, then increase the bonus
             if (pos.empty(blockSq))
