f65d26ba8278528f1bd42e6b46e2c23d1c132fd0
31m059 <37052095+31m059@users.noreply.github.com>
2018-09-02 16:58:59 -0400

    Mob=3 penalty even if mob=4. Bench: 4372291

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -381,11 +381,11 @@ namespace {
                 score += RookOnFile[bool(pe->semiopen_file(Them, file_of(s)))];
 
             // Penalty when trapped by the king, even more if the king cannot castle
-            else if (mob <= 3)
+            else if (mob <= 4)
             {
                 File kf = file_of(pos.square<KING>(Us));
                 if ((kf < FILE_E) == (file_of(s) < kf))
-                    score -= (TrappedRook - make_score(mob * 22, 0)) * (1 + !pos.can_castle(Us));
+                    score -= (TrappedRook - make_score(std::min(mob, 3) * 22, 0)) * (1 + !pos.can_castle(Us));
             }
         }
 
