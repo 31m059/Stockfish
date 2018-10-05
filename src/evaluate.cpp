a2498c5b23a0b2473c82acbb2d0492370261e6e7
31m059 <37052095+31m059@users.noreply.github.com>
2018-10-05 04:21:39 -0400

    Divide by 8, not 10. Bench: 4350838

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -323,7 +323,7 @@ namespace {
 
         mobility[Us] += MobilityBonus[Pt - 2][mob];
         if (Pt == BISHOP && pos.count<ROOK>(Them) > pos.count<ROOK>(Us))
-            mobility[Us] += MobilityBonus[Pt - 2][mob] / 10;
+            mobility[Us] += MobilityBonus[Pt - 2][mob] / 8;
 
         if (Pt == BISHOP || Pt == KNIGHT)
         {
