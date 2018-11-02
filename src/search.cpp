1519a00dbba74d86549f46fb5e0c5da196f3df5a
31m059 <37052095+31m059@users.noreply.github.com>
2018-11-02 09:37:06 -0400

    Depth < 15. Bench: 3759582

--- src/search.cpp
+++ src/search.cpp
@@ -744,7 +744,7 @@ namespace {
                         p < 0 ? (-p + 2500) / 512 : 0;
 
             pureStaticEval = evaluate(pos);
-            if (pos.count<QUEEN>() == 1 && depth < 10)
+            if (pos.count<QUEEN>() == 1 && depth < 15)
             {
                 Material::Entry* me = Material::probe(pos);
                 pureStaticEval += ((pos.key() & 7) - 3) * int(PHASE_MIDGAME - me->game_phase()) / int(PHASE_MIDGAME);
