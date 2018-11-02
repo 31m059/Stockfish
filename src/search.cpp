8ab6c942b804f077c2d63ae68530b810e39ff1cd
31m059 <37052095+31m059@users.noreply.github.com>
2018-11-02 17:16:32 -0400

    Don't change pureStaticEval. Bench: 3618540

--- src/search.cpp
+++ src/search.cpp
@@ -752,6 +752,14 @@ namespace {
         tte->save(posKey, VALUE_NONE, BOUND_NONE, DEPTH_NONE, MOVE_NONE, pureStaticEval);
     }
 
+     if (pos.count<QUEEN>() == 1 && ss->ply < 15 * ONE_PLY)
+     {
+        Material::Entry* me = Material::probe(pos);
+        int noise = ((pos.key() & 8) - 3) * int(PHASE_MIDGAME - me->game_phase()) / int(PHASE_MIDGAME);
+        ss->staticEval += noise;
+        eval += noise;
+     }
+
     // Step 7. Razoring (~2 Elo)
     if (   depth < 2 * ONE_PLY
         && eval <= alpha - RazorMargin)
