d8c9dbcb08567089bb1b900d53951c46acc4d04d
31m059 <37052095+31m059@users.noreply.github.com>
2018-12-01 02:43:20 -0500

    Compensate: reduce constant term. Bench: 3511000

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -473,14 +473,14 @@ namespace {
 
         kingDanger +=        kingAttackersCount[Them] * kingAttackersWeight[Them]
                      +  69 * kingAttacksCount[Them]
-                     + 150 * popcount(kingRing[Us] & weak)
+                     + 185 * popcount(kingRing[Us] & weak)
                      +  50 * popcount(kingRing[Us] & weak & attackedBy2[Them])
                      + 150 * popcount(pos.blockers_for_king(Us) | unsafeChecks)
                      +       tropism * tropism / 4
                      - 873 * !pos.count<QUEEN>(Them)
                      -   6 * mg_value(score) / 8
                      +       mg_value(mobility[Them] - mobility[Us])
-                     -   30;
+                     -   40;
 
         // Transform the kingDanger units into a Score, and subtract it from the evaluation
         if (kingDanger > 0)
