b34c4222485f1e512609377635973389888f219e
31m059 <37052095+31m059@users.noreply.github.com>
2018-11-30 23:29:16 -0500

    S(0, 15). Bench: 3399071

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -162,7 +162,7 @@ namespace {
   constexpr Score MinorBehindPawn    = S( 16,  0);
   constexpr Score Overload           = S( 12,  6);
   constexpr Score PawnlessFlank      = S( 18, 94);
-  constexpr Score QueenRepulsion     = S( 25,  0);
+  constexpr Score QueenRepulsion     = S(  0, 15);
   constexpr Score RestrictedPiece    = S(  7,  6);
   constexpr Score RookOnPawn         = S( 10, 28);
   constexpr Score SliderOnQueen      = S( 49, 21);
