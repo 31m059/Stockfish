daeb5538bd16c8e3de168185ee12699de72cea28
31m059 <37052095+31m059@users.noreply.github.com>
2018-12-16 01:09:38 -0500

    No transparency through K-pinned pieces. Bench: 3468155

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -298,8 +298,9 @@ namespace {
     while ((s = *pl++) != SQ_NONE)
     {
         // Find attacked squares, including x-ray attacks for bishops and rooks
-        b = Pt == BISHOP ? attacks_bb<BISHOP>(s, pos.pieces() ^ pos.pieces(QUEEN))
-          : Pt ==   ROOK ? attacks_bb<  ROOK>(s, pos.pieces() ^ pos.pieces(QUEEN) ^ pos.pieces(Us, ROOK))
+        Bitboard kingPinned = pos.pieces(Us) & pos.blockers_for_king(Us);
+        b = Pt == BISHOP ? attacks_bb<BISHOP>(s, pos.pieces() ^ (pos.pieces(QUEEN) & ~kingPinned))
+          : Pt ==   ROOK ? attacks_bb<  ROOK>(s, pos.pieces() ^ ((pos.pieces(QUEEN) | pos.pieces(Us, ROOK)) & ~kingPinned))
                          : pos.attacks_from<Pt>(s);
 
         if (pos.blockers_for_king(Us) & s)
