15166f0d1c926facb4595b5c0d6a597b5b78aa5b
31m059 <37052095+31m059@users.noreply.github.com>
2018-12-27 15:57:04 -0500

    Retire CloseEnemies. Bench: 3520923

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -153,7 +153,6 @@ namespace {
 
   // Assorted bonuses and penalties
   constexpr Score BishopPawns        = S(  3,  7);
-  constexpr Score CloseEnemies       = S(  8,  0);
   constexpr Score CorneredBishop     = S( 50, 50);
   constexpr Score Hanging            = S( 69, 36);
   constexpr Score KingProtector      = S(  7,  8);
@@ -481,9 +480,6 @@ namespace {
     // Penalty when our king is on a pawnless flank
     if (!(pos.pieces(PAWN) & kingFlank))
         score -= PawnlessFlank;
-
-    // King tropism bonus, to anticipate slow motion attacks on our king
-    score -= CloseEnemies * tropism;
 
     if (T)
         Trace::add(KING, Us, score);
