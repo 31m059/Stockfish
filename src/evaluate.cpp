df557aa4e4f067c9737401deed594582368711d0
31m059 <37052095+31m059@users.noreply.github.com>
2019-01-07 01:27:37 -0500

    Respin this idea from June 2018. Bench: 3184135

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -668,6 +668,11 @@ namespace {
                 else if (defendedSquares & blockSq)
                     k += 4;
 
+                // If protected by more than one rook or queen, assign extra bonus.
+                if (   k > 0
+                    && more_than_one(file_bb(s) & pos.pieces(Us, ROOK, QUEEN)))
+                    k += 2;
+
                 bonus += make_score(k * w, k * w);
             }
         } // rank > RANK_3
