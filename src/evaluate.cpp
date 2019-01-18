2b159e89c2ff195c6132662d293b28a3be893220
31m059 <37052095+31m059@users.noreply.github.com>
2019-01-18 14:41:16 -0500

    Max 10. Bench: 4135690

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -483,7 +483,7 @@ namespace {
         score -= PawnlessFlank;
 
     // King tropism bonus, to anticipate slow motion attacks on our king
-    score -= CloseEnemies * std::min(tropism, 12);
+    score -= CloseEnemies * std::min(tropism, 10);
 
     if (T)
         Trace::add(KING, Us, score);
