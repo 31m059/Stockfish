c3755476e525cdcdab331c7200b85156a4eea753
31m059 <37052095+31m059@users.noreply.github.com>
2019-01-25 17:33:33 -0500

    Double effect. Bench: 4142998

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -343,7 +343,7 @@ namespace {
                                      * (1 + popcount(blocked & CenterFiles));
                 // Bonus for having a bishop when we have 7th-rank passed pawns
                 constexpr Bitboard TRank7BB = (Us == WHITE ? Rank7BB : Rank2BB);
-                score += BishopPawns * popcount(TRank7BB & pos.pieces(Us, PAWN));
+                score += BishopPawns * 2 * popcount(TRank7BB & pos.pieces(Us, PAWN));
 
                 // Bonus for bishop on a long diagonal which can "see" both center squares
                 if (more_than_one(attacks_bb<BISHOP>(s, pos.pieces(PAWN)) & Center))
