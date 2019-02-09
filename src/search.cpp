19212687c774f55d0b08c7a045f50472a122ed30
31m059 <37052095+31m059@users.noreply.github.com>
2019-02-09 03:37:40 -0500

    Pawn in front of king. Inc. neighbors. Bench: 3257984

--- src/search.cpp
+++ src/search.cpp
@@ -1047,8 +1047,8 @@ moves_loop: // When in check, search starts from here
 
               // Less reduction for pawn moves near the king
               if (   type_of(movedPiece) == PAWN
-                  && pos.non_pawn_material(us) > RookValueMg + 2 * KnightValueMg
-                  && file_of(to_sq(move)) == file_of(pos.square<KING>(~us))
+                  && relative_rank(us, to_sq(move)) < relative_rank(us, pos.square<KING>(~us))
+                  && std::abs(file_of(to_sq(move)) - file_of(pos.square<KING>(~us))) <= 1
                   && std::abs(rank_of(to_sq(move)) - rank_of(pos.square<KING>(~us))) <= 3)
                   r -= ONE_PLY;
 
