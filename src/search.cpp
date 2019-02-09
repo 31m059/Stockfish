9afeb2263a7e3201866ae02864607e2ce33d09c6
31m059 <37052095+31m059@users.noreply.github.com>
2019-02-09 14:16:15 -0500

    Stricter NPM. Bench: 3580541

--- src/search.cpp
+++ src/search.cpp
@@ -1047,10 +1047,10 @@ moves_loop: // When in check, search starts from here
 
               // Less reduction for pawn moves near the king
               if (   type_of(movedPiece) == PAWN
-                  && pos.non_pawn_material(us) > RookValueMg + 2 * KnightValueMg
+                  && pos.non_pawn_material(us) > 2 * RookValueMg
                   && std::abs(file_of(to_sq(move)) - file_of(pos.square<KING>(~us))) <= 1
                   && std::abs(rank_of(to_sq(move)) - rank_of(pos.square<KING>(~us))) <= 3)
-                  r -= 2 * ONE_PLY;
+                  r -= ONE_PLY;
 
               // Decrease reduction for moves that escape a capture. Filter out
               // castling moves, because they are coded as "king captures rook" and
