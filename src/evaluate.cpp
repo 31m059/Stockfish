1744859e46950aeb825f257aa7cc1adf43eaa989
31m059 <37052095+31m059@users.noreply.github.com>
2019-02-14 01:46:53 -0500

    Double effect. Bench: 3612155

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -690,7 +690,7 @@ namespace {
             if (((attackedBy2[Us] & ~attackedBy2[Them])
                | (attackedBy[Us][ALL_PIECES] & ~attackedBy[Them][ALL_PIECES])
                |  attackedBy[Us][PAWN]) & s)
-               bonus += bonus / 16;
+               bonus += bonus / 8;
             
             bonus = bonus / 2;
         }
