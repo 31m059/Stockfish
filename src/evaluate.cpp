6e3df57c2ed1484e6f855315a8be6044a4a4fdd6
31m059 <37052095+31m059@users.noreply.github.com>
2019-02-17 03:51:47 -0500

    Half effect. Bench: 3983032

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -376,9 +376,7 @@ namespace {
                 File kf = file_of(pos.square<KING>(Us));
                 bool fawn = attackedBy[Them][PAWN] & (attackedBy[Us][KING] | b) & (Us == WHITE ? Rank2BB : Rank7BB);
                 if ((kf < FILE_E) == (file_of(s) < kf))
-                {
-                    score -= TrappedRook * (1 + !pos.castling_rights(Us)) * (1 + fawn);
-                }
+                    score -= TrappedRook * (1 + !pos.castling_rights(Us)) * (2 + fawn) / 2;
             }
         }
 
