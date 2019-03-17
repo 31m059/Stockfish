412c579c932c9fb9c056c8db74d178e9ba60a196
31m059 <37052095+31m059@users.noreply.github.com>
2019-03-17 03:04:58 -0400

    Cleanup. Bench: 3133121

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -573,7 +573,7 @@ namespace {
     b |= shift<Up>(b & TRank3BB) & ~pos.pieces();
 
     // Keep only the squares which are relatively safe
-    b &= ~attackedBy[Them][PAWN] & (safe | shift<Up>(verticalSlider[Us] & pos.pieces(Us, PAWN)));;
+    b &= ~attackedBy[Them][PAWN] & (safe | shift<Up>(verticalSlider[Us] & pos.pieces(Us, PAWN)));
 
     // Bonus for safe pawn threats on the next move
     b = pawn_attacks_bb<Us>(b) & pos.pieces(Them);
