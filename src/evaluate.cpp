c5700b9474253eb9061f6d349874e747020d0637
31m059 <37052095+31m059@users.noreply.github.com>
2019-03-29 18:12:14 -0400

    Require not doubly defended. Bench: 3464921

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -580,7 +580,7 @@ namespace {
     if (pos.count<QUEEN>(Them) == 1)
     {
         Square s = pos.square<QUEEN>(Them);
-        safe = mobilityArea[Us] & ~stronglyProtected;
+        safe = mobilityArea[Us] & ~stronglyProtected & ~attackedBy2[Them];
 
         b = attackedBy[Us][KNIGHT] & pos.attacks_from<KNIGHT>(s);
 
