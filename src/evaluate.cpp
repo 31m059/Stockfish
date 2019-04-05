83fb61786b0a75c890f6c4a6250c5dc9426966d8
31m059 <37052095+31m059@users.noreply.github.com>
2019-04-05 02:50:56 -0400

    Cutoff 512. Bench: 3260177

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -475,7 +475,7 @@ namespace {
                  -   25;
 
     // Transform the kingDanger units into a Score, and subtract it from the evaluation
-    if (kingDanger >= 256)
+    if (kingDanger >= 512)
         score -= make_score(kingDanger * kingDanger / 4096, kingDanger / 16);
 
     // Penalty when our king is on a pawnless flank
