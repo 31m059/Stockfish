1bd214cfae4721bc21b7f997345c296b2f8bd3af
31m059 <37052095+31m059@users.noreply.github.com>
2019-04-07 04:50:42 -0400

    EG-only bonus. Bench: 3153387

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -475,8 +475,10 @@ namespace {
                  -   25;
 
     // Transform the kingDanger units into a Score, and subtract it from the evaluation
-    if (kingDanger > -100)
-        score -= make_score(kingDanger * abs(kingDanger) / 4096, kingDanger / 16);
+    if (kingDanger > 0)
+        score -= make_score(kingDanger * kingDanger / 4096, kingDanger / 16);
+    else if (kingDanger > -100)
+        score -= make_score(0, kingDanger / 16);
 
     // Penalty when our king is on a pawnless flank
     if (!(pos.pieces(PAWN) & KingFlank[file_of(ksq)]))
