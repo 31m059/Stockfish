5698cadde80594f0b3c781c712f59758e98339c7
31m059 <37052095+31m059@users.noreply.github.com>
2019-04-20 19:23:21 -0400

    Also if capture. Bench: 3273965

--- src/search.cpp
+++ src/search.cpp
@@ -930,7 +930,7 @@ moves_loop: // When in check, search starts from here
 
       // Passed pawn extension
       else if (   move == ss->killers[0]
-               && pos.advanced_pawn_push(move)
+               && (captureOrPromotion || pos.advanced_pawn_push(move))
                && pos.pawn_passed(us, to_sq(move)))
           extension = ONE_PLY;
 
