3e5e2df9399acd96161c680d991481846611383f
31m059 <37052095+31m059@users.noreply.github.com>
2019-04-21 23:02:55 -0400

    Other term. Bench: 2898311

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -328,8 +328,8 @@ namespace {
                 // bishop, bigger when the center files are blocked with pawns.
                 Bitboard blocked = pos.pieces(Us, PAWN) & shift<Down>(pos.pieces());
 
-                score -= BishopPawns * pos.pawns_on_same_color_squares(Us, s)
-                                     * (1 + (popcount(blocked & CenterFiles & forward_ranks_bb(Us, s)) * 3 + popcount(blocked & CenterFiles & ~forward_ranks_bb(Us, s))) / 2);
+                score -= BishopPawns * popcount((DarkSquares & s ? DarkSquares : ~DarkSquares) & pos.pieces(Us, PAWN) & ~forward_ranks_bb(Them, s))
+                                     * (1 + popcount(blocked & CenterFiles));
 
                 // Bonus for bishop on a long diagonal which can "see" both center squares
                 if (more_than_one(attacks_bb<BISHOP>(s, pos.pieces(PAWN)) & Center))
