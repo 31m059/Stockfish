82d1a89731267438174fb20a69f1f67c0967dfb3
31m059 <37052095+31m059@users.noreply.github.com>
2019-04-22 18:25:00 -0400

    Extend killerApp also if check. Bench: 3613543

--- src/search.cpp
+++ src/search.cpp
@@ -931,7 +931,7 @@ moves_loop: // When in check, search starts from here
       // Passed pawn extension
       else if (   move == ss->killers[0]
                && pos.advanced_pawn_push(move)
-               && pos.pawn_passed(us, to_sq(move)))
+               && (pos.pawn_passed(us, to_sq(move)) || givesCheck))
           extension = ONE_PLY;
 
       // Calculate new depth for this move
