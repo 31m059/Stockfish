df48614fe454689bd442b22ac374375c7e1b3521
31m059 <37052095+31m059@users.noreply.github.com>
2019-04-23 13:25:16 -0400

    NPM >= RRN. Bench: 3446186

--- src/search.cpp
+++ src/search.cpp
@@ -931,7 +931,7 @@ moves_loop: // When in check, search starts from here
       // Passed pawn extension
       else if (   move == ss->killers[0]
                && pos.advanced_pawn_push(move)
-               && (pos.pawn_passed(us, to_sq(move)) || givesCheck))
+               && (pos.pawn_passed(us, to_sq(move)) || (givesCheck && pos.non_pawn_material(us) >= 2 * RookValueMg + KnightValueMg)))
           extension = ONE_PLY;
 
       // Calculate new depth for this move
