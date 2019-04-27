2a108921d29878eeddf6c138ed8615df282367ec
31m059 <37052095+31m059@users.noreply.github.com>
2019-04-27 03:32:06 -0400

    Double effect. Bench: 3242669

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -476,7 +476,7 @@ namespace {
                  -   7;
 
     // Transform the kingDanger units into a Score, and subtract it from the evaluation
-    if (kingDanger > 150)
+    if (kingDanger > 200)
         score -= make_score(kingDanger * kingDanger / 4096, kingDanger / 16);
 
     // Penalty when our king is on a pawnless flank
