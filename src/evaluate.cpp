d9f5924825c418339f36958fd7557a0fcb121d35
31m059 <37052095+31m059@users.noreply.github.com>
2019-05-13 03:50:44 -0400

    Divisor 4. Bench: 3228928

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -530,7 +530,7 @@ namespace {
             Square s = pop_lsb(&b);
             score += ThreatByMinor[type_of(pos.piece_on(s))];
             if (type_of(pos.piece_on(s)) != PAWN)
-                score += ThreatByRank * (int) (relative_rank(Them, s) / (attackedBy2[Them] & ~attackedBy2[Us] ? 2 : 1));
+                score += ThreatByRank * (int) (relative_rank(Them, s) / (attackedBy2[Them] & ~attackedBy2[Us] ? 4 : 1));
         }
 
         b = weak & attackedBy[Us][ROOK];
