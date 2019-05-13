ba96a9a673da7d5f3f2ca0bbdfccf70b407dc5be
31m059 <37052095+31m059@users.noreply.github.com>
2019-05-13 01:03:48 -0400

    Half TBR if 2x defended but not 2x attacked. Bench: 3111335

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -530,7 +530,7 @@ namespace {
             Square s = pop_lsb(&b);
             score += ThreatByMinor[type_of(pos.piece_on(s))];
             if (type_of(pos.piece_on(s)) != PAWN)
-                score += ThreatByRank * (int)relative_rank(Them, s);
+                score += ThreatByRank * (int) (relative_rank(Them, s) / (attackedBy2[Them] & ~attackedBy2[Us] ? 2 : 1));
         }
 
         b = weak & attackedBy[Us][ROOK];
@@ -539,7 +539,7 @@ namespace {
             Square s = pop_lsb(&b);
             score += ThreatByRook[type_of(pos.piece_on(s))];
             if (type_of(pos.piece_on(s)) != PAWN)
-                score += ThreatByRank * (int)relative_rank(Them, s);
+                score += ThreatByRank * (int) (relative_rank(Them, s) / (attackedBy2[Them] & ~attackedBy2[Us] ? 2 : 1));
         }
 
         if (weak & attackedBy[Us][KING])
