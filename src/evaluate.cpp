e5273d7bfc1e9a4d1ae2b119fd30fcdad22fd3a1
31m059 <37052095+31m059@users.noreply.github.com>
2019-06-03 03:28:27 -0400

    Half effect. Bench: 3679685

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -598,7 +598,7 @@ namespace {
         {
         Bitboard queenColor = bool(DarkSquares & s) ? DarkSquares : ~DarkSquares;
         if (pos.pieces(Us, BISHOP) & queenColor)
-            score += make_score(15, -9);
+            score += make_score(7, -4);
         }
     }
 
