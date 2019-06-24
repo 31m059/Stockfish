133f6c14d11b6002d445427045983462a5520bd4
31m059 <37052095+31m059@users.noreply.github.com>
2019-06-24 04:00:01 -0400

    Tweak. Bench: 3788751

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -458,8 +458,8 @@ namespace {
     int kingFlankAttacks = popcount(b1) + popcount(b2);
 
     kingDanger +=        kingAttackersCount[Them] * kingAttackersWeight[Them]
-                 +  62 * kingAttacksCount[Them]
-                 + 185 * popcount(kingRing[Us] & weak)
+                 +  69 * kingAttacksCount[Them]
+                 + 169 * popcount(kingRing[Us] & weak)
                  - 100 * bool(attackedBy[Us][KNIGHT] & attackedBy[Us][KING])
                  -  35 * bool(attackedBy[Us][BISHOP] & attackedBy[Us][KING])
                  + 150 * popcount(pos.blockers_for_king(Us) | unsafeChecks)
