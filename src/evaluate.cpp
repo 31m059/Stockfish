d274aa79e91e06db03175aafe85766812da82552
31m059 <37052095+31m059@users.noreply.github.com>
2019-06-24 04:02:36 -0400

    Tweak. Bench: 3665715

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -464,10 +464,10 @@ namespace {
                  -  35 * bool(attackedBy[Us][BISHOP] & attackedBy[Us][KING])
                  + 150 * popcount(pos.blockers_for_king(Us) | unsafeChecks)
                  - 873 * !pos.count<QUEEN>(Them)
-                 -  10 * mg_value(score) / 8
+                 -   6 * mg_value(score) / 8
                  +       mg_value(mobility[Them] - mobility[Us])
                  +   5 * kingFlankAttacks * kingFlankAttacks / 16
-                 -   7;
+                 -  20;
 
     // Transform the kingDanger units into a Score, and subtract it from the evaluation
     if (kingDanger > 100)
