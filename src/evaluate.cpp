3efa6d0476ee29cc3e33f4da555931f91faa7e8c
31m059 <37052095+31m059@users.noreply.github.com>
2019-07-02 01:38:46 -0400

    Require weak landing square. Bench: 3496616

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -666,14 +666,14 @@ namespace {
         bool blockingRook = false;
 
         Bitboard weak = (attackedBy[Them][ALL_PIECES] & ~attackedBy2[Us]) | attackedBy2[Them];
-        if (weak & s && (weak | pos.pieces()) & blockSq)
+        if (r >= RANK_5)
         {
             bb = pos.pieces(Us, ROOK) & forward_file_bb(Us, s);
 
             while (bb)
             {
                 Square rookSquare = pop_lsb(&bb);
-                blockingRook |= !(rank_bb(rookSquare) & attacks_bb<ROOK>(rookSquare, pos.pieces()) & attacks_bb<ROOK>(pos.square<KING>(Them), pos.pieces()));
+                blockingRook |= !(~weak & rank_bb(rookSquare) & attacks_bb<ROOK>(rookSquare, pos.pieces()) & attacks_bb<ROOK>(pos.square<KING>(Them), pos.pieces()));
             }
         }
 
