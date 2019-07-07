cb0a6ba163c59e755d2a436209ce8793c6ca6856
31m059 <37052095+31m059@users.noreply.github.com>
2019-07-07 01:25:42 -0400

    Less EG passer bonus if stopped by K. Bench: 3577723

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -659,7 +659,7 @@ namespace {
                 if (defendedSquares & blockSq)
                     k += 5;
 
-                bonus += make_score(k * w, k * w);
+                bonus += make_score(k * w, std::max(k - more_than_one(attackedBy[Them][KING] & squaresToQueen), 0) * w);
             }
         } // r > RANK_3
 
