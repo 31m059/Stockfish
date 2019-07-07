a5af5c2f668789730cae4128edabe5ffadccf305
31m059 <37052095+31m059@users.noreply.github.com>
2019-07-07 04:00:56 -0400

    k -= 20. Bench: 3714300

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -659,7 +659,7 @@ namespace {
                 if (defendedSquares & blockSq)
                     k += 5;
 
-                bonus += make_score(k * w, k * w / (more_than_one(attackedBy[Them][KING] & squaresToQueen) ? 2 : 1));
+                bonus += make_score(k * w, std::max(k - 20 * more_than_one(attackedBy[Them][KING] & squaresToQueen), 0) * w);
             }
         } // r > RANK_3
 
