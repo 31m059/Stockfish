7071482501da4979d101f2ffcb3a29a68267a54a
31m059 <37052095+31m059@users.noreply.github.com>
2019-07-09 22:47:57 -0400

    Not if they have a rook on the file. Bench: 3729841

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -650,7 +650,8 @@ namespace {
                 // If there are no enemy attacks on passed pawn span, assign a big bonus.
                 // Otherwise assign a smaller bonus if the path to queen is not attacked
                 // and even smaller bonus if it is attacked but block square is not.
-                bool blockSqOnly = (unsafeSquares & blockSq) == (unsafeSquares & squaresToQueen);
+                bool blockSqOnly = (unsafeSquares & blockSq) == (unsafeSquares & squaresToQueen)
+                                && !(pos.pieces(Them, ROOK) & file_bb(s));
                 int k = !unsafeSquares                    ? 35 :
                         !(unsafeSquares & squaresToQueen) ? 20 :
                         !(unsafeSquares & blockSq)        ?  9 :
