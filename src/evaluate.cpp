a5f4c238006e4944c4edf4aa6f98b437ed9cdf33
31m059 <37052095+31m059@users.noreply.github.com>
2019-07-12 13:44:28 -0400

    += 15. Bench: 3688035

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -657,7 +657,7 @@ namespace {
 
                 // Assign a larger bonus if the block square is defended
                 if (defendedSquares & blockSq)
-                    k += (attackedBy2[Us] & blockSq ? 10 : 5);
+                    k += (attackedBy2[Us] & blockSq ? 15 : 5);
 
                 bonus += make_score(k * w, k * w);
             }
