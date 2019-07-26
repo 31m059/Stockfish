a5bb4e463cdbec27a91e799109e5d11cd55f022e
31m059 <37052095+31m059@users.noreply.github.com>
2019-07-26 13:12:53 -0400

    Friendly king, 1/2 effect. Bench: 3464656

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -629,7 +629,8 @@ namespace {
                 bonus -= make_score(0, king_proximity(Us, blockSq + Up) * w);
 
             // If the pawn is free to advance, then increase the bonus
-            if ((~pos.pieces() | pos.pieces(Them, KING)) & blockSq)
+            int k  = 0;
+            if ((~pos.pieces() | pos.pieces(Us, KING)) & blockSq)
             {
                 squaresToQueen = forward_file_bb(Us, s);
                 unsafeSquares = passed_pawn_span(Us, s);
@@ -642,17 +643,21 @@ namespace {
                 // If there are no enemy attacks on passed pawn span, assign a big bonus.
                 // Otherwise assign a smaller bonus if the path to queen is not attacked
                 // and even smaller bonus if it is attacked but block square is not.
-                int k = !unsafeSquares                    ? 35 :
-                        !(unsafeSquares & squaresToQueen) ? 20 :
-                        !(unsafeSquares & blockSq)        ?  9 :
-                                                             0 ;
+                k = !unsafeSquares                    ? 35 :
+                    !(unsafeSquares & squaresToQueen) ? 20 :
+                    !(unsafeSquares & blockSq)        ?  9 :
+                                                         0 ;
 
                 // Assign a larger bonus if the block square is defended
                 if ((pos.pieces(Us) & bb) || (attackedBy[Us][ALL_PIECES] & blockSq))
                     k += 5;
 
-                bonus += make_score(k * w, k * w);
+                
             }
+            if (pos.pieces(Us, KING) & blockSq)
+                k /= 2;
+            
+            bonus += make_score(k * w, k * w);
         } // r > RANK_3
 
         // Scale down bonus for candidate passers which need more than one
