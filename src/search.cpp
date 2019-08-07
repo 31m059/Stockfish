6fcd36b5fef7b190b22d3c0a232bfb55910e2798
31m059 <37052095+31m059@users.noreply.github.com>
2019-08-07 01:49:39 -0400

    Remove statScore condition. Bench: 4088015

--- src/search.cpp
+++ src/search.cpp
@@ -796,7 +796,6 @@ namespace {
     // Step 9. Null move search with verification search (~40 Elo)
     if (   !PvNode
         && (ss-1)->currentMove != MOVE_NULL
-        && (ss-1)->statScore < 22661
         &&  eval >= beta
         &&  ss->staticEval >= beta - 33 * depth / ONE_PLY + 299
         && !excludedMove
