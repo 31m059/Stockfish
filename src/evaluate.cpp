19ac0dd0e86d41043bd2f8ac87a781bc9dfb5957
31m059 <37052095+31m059@users.noreply.github.com>
2019-08-14 18:43:59 -0400

    Only p-blocked pawns. S(0, 10). Bench: 4073433

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -121,7 +121,7 @@ namespace {
   constexpr Score ThreatByRook[PIECE_TYPE_NB] = {
     S(0, 0), S(0, 24), S(38, 71), S(38, 61), S(0, 38), S(51, 38)
   };
-  constexpr Score ThreatByRookBlockedPawn  = S(20, 20);
+  constexpr Score ThreatByRookBlockedPawn  = S(0, 10);
 
   // PassedRank[Rank] contains a bonus according to the rank of a passed pawn
   constexpr Score PassedRank[RANK_NB] = {
@@ -515,7 +515,7 @@ namespace {
         {
             Square s = pop_lsb(&b);
             score += ThreatByMinor[type_of(pos.piece_on(s))];
-            if (pos.pieces(PAWN) & shift<Up>(pos.pieces()) & s)
+            if (pos.pieces(PAWN) & shift<Up>(pos.pieces(PAWN)) & s)
                 score += ThreatByMinorBlockedPawn;
             if (type_of(pos.piece_on(s)) != PAWN)
                 score += ThreatByRank * (int)relative_rank(Them, s);
@@ -526,7 +526,7 @@ namespace {
         {
             Square s = pop_lsb(&b);
             score += ThreatByRook[type_of(pos.piece_on(s))];
-            if (pos.pieces(PAWN) & shift<Up>(pos.pieces()) & s)
+            if (pos.pieces(PAWN) & shift<Up>(pos.pieces(PAWN)) & s)
                 score += ThreatByRookBlockedPawn;
             if (type_of(pos.piece_on(s)) != PAWN)
                 score += ThreatByRank * (int)relative_rank(Them, s);
