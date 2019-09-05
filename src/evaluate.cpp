f4e5c78f46b7cedf566ff73d6dd2b2f6cffa110b
31m059 <37052095+31m059@users.noreply.github.com>
2019-09-05 18:19:15 -0400

    Correct direction. Bench: 3868824

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -459,7 +459,8 @@ namespace {
                  -   6 * mg_value(score) / 8
                  +       mg_value(mobility[Them] - mobility[Us])
                  +   5 * kingFlankAttacks * kingFlankAttacks / 16
-                 -  12;
+                 -   2;
+    dbg_mean_of(kingDanger);
 
     // Transform the kingDanger units into a Score, and subtract it from the evaluation
     if (kingDanger > 100)
