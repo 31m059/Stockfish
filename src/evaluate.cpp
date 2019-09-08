42be739cdef56e960097d6dfaf41b4d846f23a4a
31m059 <37052095+31m059@users.noreply.github.com>
2019-09-08 01:53:13 -0400

    Tangent at 400 cp. Bench: 4026852

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -462,8 +462,11 @@ namespace {
                  -   7;
 
     // Transform the kingDanger units into a Score, and subtract it from the evaluation
-    if (kingDanger > 100)
+    if (kingDanger > 1280)
+        score -= make_score(kingDanger * 5 / 8 - 400, kingDanger / 16);
+    else if (kingDanger > 100)
         score -= make_score(kingDanger * kingDanger / 4096, kingDanger / 16);
+    
 
     // Penalty when our king is on a pawnless flank
     if (!(pos.pieces(PAWN) & KingFlank[file_of(ksq)]))
