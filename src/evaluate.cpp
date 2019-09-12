86af318b8107dc5d2342557c1872de32cb8f30d7
31m059 <37052095+31m059@users.noreply.github.com>
2019-09-12 18:07:00 -0400

    Exclude KQ of either color. Bench: 3648201

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -228,7 +228,7 @@ namespace {
 
     // Squares occupied by those pawns, by our king or queen or controlled by
     // enemy pawns are excluded from the mobility area.
-    mobilityArea[Us] = ~(b | pos.pieces(Us, KING, QUEEN) | pe->pawn_attacks(Them));
+    mobilityArea[Us] = ~(b | pos.pieces(KING, QUEEN) | pe->pawn_attacks(Them));
 
     // Initialize attackedBy[] for king and pawns
     attackedBy[Us][KING] = pos.attacks_from<KING>(ksq);
