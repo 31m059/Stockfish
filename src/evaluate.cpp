3a1095195fca89e72bb7f5231af54a9abbaff34a
31m059 <37052095+31m059@users.noreply.github.com>
2019-09-14 00:33:50 -0400

    Extra if blocker pawn is pawn-defended and unattackable. Bench: 3883140

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -317,9 +317,10 @@ namespace {
                 // Penalty according to number of pawns on the same color square as the
                 // bishop, bigger when the center files are blocked with pawns.
                 Bitboard blocked = pos.pieces(Us, PAWN) & shift<Down>(pos.pieces());
+                Bitboard blocked2 = pos.pieces(Us, PAWN) & shift<Down>(pos.pieces(Them, PAWN) & attackedBy[Them][PAWN] & ~pe->pawn_attacks_span(Us));
 
                 score -= BishopPawns * pos.pawns_on_same_color_squares(Us, s)
-                                     * (1 + popcount(blocked & CenterFiles));
+                                     * (1 + popcount(blocked & CenterFiles) + popcount(blocked2 & CenterFiles));
 
                 // Bonus for bishop on a long diagonal which can "see" both center squares
                 if (more_than_one(attacks_bb<BISHOP>(s, pos.pieces(PAWN)) & Center))
