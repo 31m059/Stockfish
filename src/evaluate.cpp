11fe2150caf5e9b24cbb7b86a2a1c4a5070b2888
31m059 <37052095+31m059@users.noreply.github.com>
2019-09-15 22:37:44 -0400

    Full effect if two factors. Bench: 3811721

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -738,8 +738,8 @@ namespace {
                     +  9 * outflanking
                     + 18 * pawnsOnBothFlanks
                     + 49 * !pos.non_pawn_material()
-                    - 18 * std::max(almostUnwinnable - 1, 0)
-                    - 94 ;
+                    - 36 * (almostUnwinnable >= 2)
+                    -103 ;
 
     // Now apply the bonus: note that we find the attacking side by extracting the
     // sign of the midgame or endgame values, and that we carefully cap the bonus
