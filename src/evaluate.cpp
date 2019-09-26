d81b7e24ec7ccce61a3ad750322c4a4bdecca1a4
31m059 <37052095+31m059@users.noreply.github.com>
2019-09-26 18:19:21 -0400

    Bugfix. S(20,20). Bench: 3561701

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -142,7 +142,7 @@ namespace {
   constexpr Score RookOnPawn         = S( 10, 32);
   constexpr Score RookOnQueenFile    = S( 11,  4);
   constexpr Score SliderOnQueen      = S( 59, 18);
-  constexpr Score StormSupport       = S( 10, 10);
+  constexpr Score StormSupport       = S( 20, 20);
   constexpr Score ThreatByKing       = S( 24, 89);
   constexpr Score ThreatByPawnPush   = S( 48, 39);
   constexpr Score ThreatByRank       = S( 13,  0);
@@ -314,8 +314,8 @@ namespace {
 
             // Penalty if the piece is far from the king
             score -= KingProtector * distance(s, pos.square<KING>(Us));
-            
-            bb =  b & StormRanks & KingFlank[file_of(pos.square<KING>(Them))] 
+
+            bb =  b & StormRanks & KingFlank[file_of(pos.square<KING>(Them))]
                 & pos.pieces(Us, PAWN) & shift<Down>(~pos.pieces() | attackedBy[Us][PAWN]);
             score += StormSupport * popcount(bb);
 
