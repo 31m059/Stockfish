eb9676462b6dca4df67e80f70459758959b8c752
31m059 <37052095+31m059@users.noreply.github.com>
2019-10-06 22:25:49 -0400

    Just our pawns. Bench: 3669504

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -304,7 +304,7 @@ namespace {
                 score += Outpost * (Pt == KNIGHT ? 2 : 1);
 
             // Knight and Bishop bonus for being right behind a pawn
-            if (shift<Down>(pos.pieces(PAWN)) & s)
+            if (shift<Down>(pos.pieces(Us, PAWN)) & s)
                 score += MinorBehindPawn;
 
             // Penalty if the piece is far from the king
