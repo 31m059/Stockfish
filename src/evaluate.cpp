fad4ee607cdd0a1eebd557bb7b6025410ff990ad
31m059 <37052095+31m059@users.noreply.github.com>
2019-10-22 14:55:41 -0400

    No TBSP, TBPP against disc check blockers. Bench: 4861131

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -535,7 +535,7 @@ namespace {
 
     // Bonus for attacking enemy pieces with our relatively safe pawns
     b = pos.pieces(Us, PAWN) & safe;
-    b = pawn_attacks_bb<Us>(b) & nonPawnEnemies;
+    b = pawn_attacks_bb<Us>(b) & nonPawnEnemies & ~pos.blockers_for_king(Us);
     score += ThreatBySafePawn * popcount(b);
 
     // Find squares where our pawns can push on the next move
@@ -546,7 +546,7 @@ namespace {
     b &= ~attackedBy[Them][PAWN] & safe;
 
     // Bonus for safe pawn threats on the next move
-    b = pawn_attacks_bb<Us>(b) & nonPawnEnemies;
+    b = pawn_attacks_bb<Us>(b) & nonPawnEnemies & ~pos.blockers_for_king(Us);
     score += ThreatByPawnPush * popcount(b);
 
     // Bonus for threats on the next moves against enemy queen
