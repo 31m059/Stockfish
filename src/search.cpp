69b9ef927a9845e788e00edd498aa9c3becc0bee
31m059 <37052095+31m059@users.noreply.github.com>
2019-10-25 03:56:14 -0400

    Half for captures. Bench: 4472018

--- src/search.cpp
+++ src/search.cpp
@@ -1108,13 +1108,17 @@ moves_loop: // When in check, search starts from here
 
           // Increase reduction for cut nodes (~5 Elo)
           if (cutNode)
-              r += 2;
+              r += 1;
 
           if (!captureOrPromotion)
           {
               // Increase reduction if ttMove is a capture (~0 Elo)
               if (ttCapture)
                   r++;
+
+              // Increase reduction for cut nodes (~5 Elo)
+              if (cutNode)
+                  r += 1;
 
               // Decrease reduction for moves that escape a capture. Filter out
               // castling moves, because they are coded as "king captures rook" and
