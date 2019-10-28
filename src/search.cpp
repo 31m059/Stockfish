3e2c20f0cd0674f76c4fe1b4a3050d200aaceb36
31m059 <37052095+31m059@users.noreply.github.com>
2019-10-28 16:09:41 -0400

    1 if capture, 2 othewise. Bench: 4575500

--- src/search.cpp
+++ src/search.cpp
@@ -1106,6 +1106,14 @@ moves_loop: // When in check, search starts from here
           // Decrease reduction if ttMove has been singularly extended
           if (singularLMR)
               r -= 2;
+          
+          // Decrease reduction for moves that escape a capture. Filter out
+          // castling moves, because they are coded as "king captures rook" and
+          // hence break make_move().
+          if (    !cutNode
+               && type_of(move) == NORMAL
+               && !pos.see_ge(reverse_move(move)))
+              r -= (captureOrPromotion ? 1 : 2);
 
           if (!captureOrPromotion)
           {
@@ -1115,14 +1123,7 @@ moves_loop: // When in check, search starts from here
 
               // Increase reduction for cut nodes (~5 Elo)
               if (cutNode)
-                  r += 2;
-
-              // Decrease reduction for moves that escape a capture. Filter out
-              // castling moves, because they are coded as "king captures rook" and
-              // hence break make_move(). (~5 Elo)
-              else if (    type_of(move) == NORMAL
-                       && !pos.see_ge(reverse_move(move)))
-                  r -= 2;
+                  r += 2;              
 
               ss->statScore =  thisThread->mainHistory[us][from_to(move)]
                              + (*contHist[0])[movedPiece][to_sq(move)]
