834e779662a14fadf9d273df6128c0dd87d7d6d8
31m059 <37052095+31m059@users.noreply.github.com>
2019-10-30 22:52:37 -0400

    Offsetting changes. Bench: 4647276

--- src/search.cpp
+++ src/search.cpp
@@ -1101,7 +1101,11 @@ moves_loop: // When in check, search starts from here
 
           // Decrease reduction if opponent's move count is high (~10 Elo)
           if ((ss-1)->moveCount > 15)
-              r--;
+              r -= 1 + captureOrPromotion;
+          
+          // Increase reduction for cut nodes
+          if (cutNode)
+              r += 1 + captureOrPromotion;
 
           // Decrease reduction if ttMove has been singularly extended
           if (singularLMR)
@@ -1112,10 +1116,6 @@ moves_loop: // When in check, search starts from here
               // Increase reduction if ttMove is a capture (~0 Elo)
               if (ttCapture)
                   r++;
-
-              // Increase reduction for cut nodes (~5 Elo)
-              if (cutNode)
-                  r += 2;
 
               // Decrease reduction for moves that escape a capture. Filter out
               // castling moves, because they are coded as "king captures rook" and
