c6dac49c0ce9d714a65d64cb1664087f535c7dcf
31m059 <37052095+31m059@users.noreply.github.com>
2019-10-30 21:11:21 -0400

    Opposite effect. Bench: 4453929

--- src/search.cpp
+++ src/search.cpp
@@ -1105,7 +1105,7 @@ moves_loop: // When in check, search starts from here
 
           // Decrease reduction if ttMove has been singularly extended
           if (singularLMR)
-              r -= 2 - captureOrPromotion;
+              r -= 2 + captureOrPromotion;
 
           if (!captureOrPromotion)
           {
