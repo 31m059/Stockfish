8a494ca7650125289246164554cebb0ce6c771c1
31m059 <37052095+31m059@users.noreply.github.com>
2019-11-10 01:41:18 -0500

    Increase sf if pawns on both flanks. Bench: 5157464

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -746,7 +746,11 @@ namespace {
             && pos.non_pawn_material() == 2 * BishopValueMg)
             sf = 16 + 4 * pe->passed_count();
         else
-            sf = std::min(sf, 36 + (pos.opposite_bishops() ? 2 : 7) * pos.count<PAWN>(strongSide));
+        {
+            bool pawnsOnBothFlanks =   (pos.pieces(PAWN) & QueenSide)
+                                    && (pos.pieces(PAWN) & KingSide);
+            sf = std::min(sf, 36 + (pos.opposite_bishops() ? 2 : 7) * (pos.count<PAWN>(strongSide) + pawnsOnBothFlanks));
+        }
 
         sf = std::max(0, sf - (pos.rule50_count() - 12) / 4);
     }
