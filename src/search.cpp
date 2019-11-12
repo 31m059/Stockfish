f439032877a14105e7df125d1ed5ee975fd840e7
31m059 <37052095+31m059@users.noreply.github.com>
2019-11-12 14:49:06 -0500

    No pruning at shallow depth in check. Bench: 5123237

--- src/search.cpp
+++ src/search.cpp
@@ -1022,6 +1022,7 @@ moves_loop: // When in check, search starts from here
 
           if (   !captureOrPromotion
               && !givesCheck
+              && !inCheck
               && (!PvNode || !pos.advanced_pawn_push(move) || pos.non_pawn_material(~us) > BishopValueMg))
           {
               // Reduced depth of the next LMR search
