831f41921be2a7d4a1af53e242d9b9c59ad8de32
31m059 <37052095+31m059@users.noreply.github.com>
2019-11-12 13:02:35 -0500

    Opposite tweak. Bench: 4540500

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -448,7 +448,7 @@ namespace {
                  + 185 * popcount(kingRing[Us] & weak)
                  + 148 * popcount(unsafeChecks)
                  +  98 * popcount(pos.blockers_for_king(Us))
-                 +  75 * kingAttacksCount[Them]
+                 +  65 * kingAttacksCount[Them]
                  +   3 * kingFlankAttacks * kingFlankAttacks / 8
                  +       mg_value(mobility[Them] - mobility[Us])
                  - 873 * !pos.count<QUEEN>(Them)
