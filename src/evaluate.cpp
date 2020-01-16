f1095606674af96ec2c3a1dae43b42daba23cf84
31m059 <37052095+31m059@users.noreply.github.com>
2020-01-16 00:24:36 -0500

    Double effect. Bench: 4840118

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -753,7 +753,7 @@ namespace {
             sf = 22 ;
         else {
             sf = std::min(sf, 36 + (pos.opposite_bishops() ? 2 : 7) * pos.count<PAWN>(strongSide) 
-                                 + pe->pawn_islands(~strongSide) - pe->pawn_islands(strongSide));
+                                 + 2 * (pe->pawn_islands(~strongSide) - pe->pawn_islands(strongSide)));
         }
 
         sf = std::max(0, sf - (pos.rule50_count() - 12) / 4);
