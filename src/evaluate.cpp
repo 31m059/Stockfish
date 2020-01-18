42b4ed05f0fe1a872419c9d2b51bf208f75a260d
31m059 <37052095+31m059@users.noreply.github.com>
2020-01-17 22:16:08 -0500

    Slightly increase scale factor if strongSide has a pawn chain. Bench: 4976070

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -752,7 +752,8 @@ namespace {
             && pos.non_pawn_material() == 2 * BishopValueMg)
             sf = 22 ;
         else
-            sf = std::min(sf, 36 + (pos.opposite_bishops() ? 2 : 7) * pos.count<PAWN>(strongSide));
+            sf = std::min(sf, 36 + (pos.opposite_bishops() ? 2 : 7) * pos.count<PAWN>(strongSide)
+                                 + (pe->chain_length(strongSide) > 2));
 
         sf = std::max(0, sf - (pos.rule50_count() - 12) / 4);
     }
