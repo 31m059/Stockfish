e564ec5e9a30dd42a46788f4fdc8a1a7ae5f5212
31m059 <37052095+31m059@users.noreply.github.com>
2020-01-18 23:35:35 -0500

    +25 if enemy pawn chain near king. Bench: 5042556

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -454,6 +454,7 @@ namespace {
                  - 100 * bool(attackedBy[Us][KNIGHT] & attackedBy[Us][KING])
                  -   6 * mg_value(score) / 8
                  -   4 * kingFlankDefense
+                 +  25 * bool(pe->pawn_chain_fronts(Them) & KingFlank[file_of(ksq)] & Camp)
                  +  37;
 
     // Transform the kingDanger units into a Score, and subtract it from the evaluation
