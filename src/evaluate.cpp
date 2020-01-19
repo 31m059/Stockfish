26205bf8254529a2d5a6936385cc51fef13b3731
31m059 <37052095+31m059@users.noreply.github.com>
2020-01-19 03:45:09 -0500

    Double effect + compensation. Bench: 5066533

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -454,8 +454,8 @@ namespace {
                  - 100 * bool(attackedBy[Us][KNIGHT] & attackedBy[Us][KING])
                  -   6 * mg_value(score) / 8
                  -   4 * kingFlankDefense
-                 + 100 * bool(pe->pawn_chain_fronts(Them) & KingFlank[file_of(ksq)] & Camp)
-                 +  37;
+                 +  50 * bool(pe->pawn_chain_fronts(Them) & KingFlank[file_of(ksq)] & Camp)
+                 +  32;
 
     // Transform the kingDanger units into a Score, and subtract it from the evaluation
     if (kingDanger > 100)
