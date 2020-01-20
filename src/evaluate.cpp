5a5c2f23f544a42453024c0055eeea6a8a1a2f09
31m059 <37052095+31m059@users.noreply.github.com>
2020-01-20 18:31:35 -0500

    Compensation instead. Bench: 4960812

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -457,8 +457,8 @@ namespace {
                  - 100 * bool(attackedBy[Us][KNIGHT] & attackedBy[Us][KING])
                  -   6 * mg_value(score) / 8
                  -   4 * kingFlankDefense
-                 +   5 * bool(b4) * adjLength * adjLength
-                 +  37;
+                 +  50 * bool(b4) * adjLength * adjLength
+                 +  23;
 
     // Transform the kingDanger units into a Score, and subtract it from the evaluation
     if (kingDanger > 100)
