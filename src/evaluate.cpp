f92f4d6ceb56c200e66be6029a9165d5475c5b15
31m059 <37052095+31m059@users.noreply.github.com>
2020-01-21 02:55:37 -0500

    Double coefficient. Bench: 4512129

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -459,8 +459,8 @@ namespace {
                  - 100 * bool(attackedBy[Us][KNIGHT] & attackedBy[Us][KING])
                  -   6 * mg_value(score) / 8
                  -   4 * kingFlankDefense
-                 +  20 * std::max(pieceDiff, 0)
-                 +  37;
+                 +  10 * pieceDiff
+                 +  43;
 
     // Transform the kingDanger units into a Score, and subtract it from the evaluation
     if (kingDanger > 100)
