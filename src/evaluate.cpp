6be8c621f80e19a0df602c58af16958c203f6f62
31m059 <37052095+31m059@users.noreply.github.com>
2020-01-20 23:52:28 -0500

    Compensation. Bench: 4583188

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -460,7 +460,7 @@ namespace {
                  -   6 * mg_value(score) / 8
                  -   4 * kingFlankDefense
                  +   5 * pieceDiff
-                 +  37;
+                 +  43;
 
     // Transform the kingDanger units into a Score, and subtract it from the evaluation
     if (kingDanger > 100)
