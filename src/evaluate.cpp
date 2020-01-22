4b16eeea563ee8acfeb5954e91c84ba728c7dbb6
31m059 <37052095+31m059@users.noreply.github.com>
2020-01-22 17:48:25 -0500

    Pawns on the edge don't count. Bench: 4689468

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -752,7 +752,7 @@ namespace {
             && pos.non_pawn_material() == 2 * BishopValueMg)
             sf = 22 ;
         else
-            sf = std::min(sf, 36 + (pos.opposite_bishops() ? 2 : 7) * pos.count<PAWN>(strongSide));
+            sf = std::min(sf, 38 + (pos.opposite_bishops() ? 2 : 7) * popcount(pos.pieces(strongSide, PAWN) & ~(FileABB | FileHBB)));
 
         sf = std::max(0, sf - (pos.rule50_count() - 12) / 4);
     }
