bfe065ab0ebdbb26e79077bf9a57c7038396d125
31m059 <37052095+31m059@users.noreply.github.com>
2020-01-23 12:31:12 -0500

    Less LMR, take 2. Bench: 5029523

--- src/search.cpp
+++ src/search.cpp
@@ -1147,7 +1147,7 @@ moves_loop: // When in check, search starts from here
 
           // Reduction if an expected PvNode, but TT move was not PV
           if (PvNode && !(ttHit && tte->is_pv()))
-              r++;
+              r--;
 
           if (!captureOrPromotion)
           {
