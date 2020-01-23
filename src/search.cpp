ba8e529cf07ea9f5cb3ffc049159e84e59f0bef9
31m059 <37052095+31m059@users.noreply.github.com>
2020-01-23 01:54:20 -0500

    PV in TT, double effect. Bench: 4779316

--- src/search.cpp
+++ src/search.cpp
@@ -1149,7 +1149,7 @@ moves_loop: // When in check, search starts from here
           {
               // Increase reduction if ttMove is a capture (~5 Elo)
               if (ttCapture)
-                  r += 1 + 2 * PvNode;
+                  r += 1 + 2 * (ttHit && tte->is_pv());
 
               // Increase reduction for cut nodes (~10 Elo)
               if (cutNode)
