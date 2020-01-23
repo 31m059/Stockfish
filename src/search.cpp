f418808e9f0a2a0e25766422f5db7a3f65617ada
31m059 <37052095+31m059@users.noreply.github.com>
2020-01-23 01:50:54 -0500

    Add the two conditions. Bench: 4450476

--- src/search.cpp
+++ src/search.cpp
@@ -1149,7 +1149,7 @@ moves_loop: // When in check, search starts from here
           {
               // Increase reduction if ttMove is a capture (~5 Elo)
               if (ttCapture)
-                  r += 1 + (ttHit && tte->is_pv());
+                  r += 1 + PvNode + (ttHit && tte->is_pv());
 
               // Increase reduction for cut nodes (~10 Elo)
               if (cutNode)
