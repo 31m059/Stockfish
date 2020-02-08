85a71b1c7a198580d098eb67a23db75ca37226a0
31m059 <37052095+31m059@users.noreply.github.com>
2020-02-08 02:16:35 -0500

    Don't prune if the TT hit is from PV. Bench: 4957473

--- src/search.cpp
+++ src/search.cpp
@@ -829,8 +829,8 @@ namespace {
               || (ss-4)->staticEval == VALUE_NONE) : ss->staticEval > (ss-2)->staticEval;
 
     // Step 8. Futility pruning: child node (~50 Elo)
-    if (   !PvNode
-        &&  depth < 6 - 2 * ttPv
+    if (   !ttPv
+        &&  depth < 6
         &&  eval - futility_margin(depth, improving) >= beta
         &&  eval < VALUE_KNOWN_WIN) // Do not return unproven wins
         return eval;
