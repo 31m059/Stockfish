c8a606b90bd6a08d4674f0236b6384a2125704ce
31m059 <37052095+31m059@users.noreply.github.com>
2020-03-03 00:29:41 -0500

    Refine LMR cutNode condition. Bench: 5076993

--- src/search.cpp
+++ src/search.cpp
@@ -1127,7 +1127,7 @@ moves_loop: // When in check, search starts from here
           && (  !captureOrPromotion
               || moveCountPruning
               || ss->staticEval + PieceValue[EG][pos.captured_piece()] <= alpha
-              || cutNode
+              || (cutNode && !ttPv)
               || thisThread->ttHitAverage < 375 * ttHitAverageResolution * ttHitAverageWindow / 1024))
       {
           Depth r = reduction(improving, depth, moveCount);
