cc124ac10baf4aee7f122cb4df17921aac261d5b
31m059 <37052095+31m059@users.noreply.github.com>
2020-03-11 04:31:51 -0400

    Check extension for killer moves. Bench: 5265792

--- src/search.cpp
+++ src/search.cpp
@@ -1078,7 +1078,7 @@ moves_loop: // When in check, search starts from here
 
       // Check extension (~2 Elo)
       else if (    givesCheck
-               && (pos.is_discovery_check_on_king(~us, move) || pos.see_ge(move)))
+               && (pos.is_discovery_check_on_king(~us, move) || pos.see_ge(move) || move == ss->killers[0]))
           extension = 1;
 
       // Passed pawn extension
