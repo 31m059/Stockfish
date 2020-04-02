43b8cab593a15373232d057caee1fa821a8a921f
31m059 <37052095+31m059@users.noreply.github.com>
2020-04-02 10:45:23 -0400

    1/3. Bench: 5101151

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -641,7 +641,7 @@ namespace {
         // Scale down bonus for candidate passers which need more than one
         // pawn push to become passed.
         if (!pos.pawn_passed(Us, s + Up))
-            bonus = bonus / 2;
+            bonus = bonus / 3;
 
         score += bonus - PassedFile * edge_distance(file_of(s));
     }
