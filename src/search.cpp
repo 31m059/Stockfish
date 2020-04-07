9375c3a972026e4124e95aedc821716396a47f2d
31m059 <37052095+31m059@users.noreply.github.com>
2020-04-07 18:42:57 -0400

    Double effect. Bench: 4684059

--- src/search.cpp
+++ src/search.cpp
@@ -1132,7 +1132,7 @@ moves_loop: // When in check, search starts from here
 
       // Step 16. Reduced depth search (LMR, ~200 Elo). If the move fails high it will be
       // re-searched at full depth.
-      int captValue = ss->staticEval + PieceValue[EG][pos.captured_piece()] + 50 * formerPv;
+      int captValue = ss->staticEval + PieceValue[EG][pos.captured_piece()] + 200 * formerPv;
       if (    depth >= 3
           &&  moveCount > 1 + 2 * rootNode
           && (!rootNode || thisThread->best_move_count(move) == 0)
