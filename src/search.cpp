f513f6c4504e86c20e9a0455a5b6cf32df3e219f
31m059 <37052095+31m059@users.noreply.github.com>
2020-04-06 20:05:57 -0400

    Don't increase reduction if ttPv. Bench: 5200492

--- src/search.cpp
+++ src/search.cpp
@@ -1197,7 +1197,7 @@ moves_loop: // When in check, search starts from here
 
             // Unless giving check, this capture is likely bad
             if (   !givesCheck
-                && !formerPv
+                && !ttPv
                 && ss->staticEval + PieceValue[EG][pos.captured_piece()] + 200 * depth <= alpha)
                 r++;
           }
