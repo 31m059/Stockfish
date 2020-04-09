cb53dc4c7cd5f79b604e5b4f071bb3505124d259
31m059 <37052095+31m059@users.noreply.github.com>
2020-04-09 15:18:52 -0400

    Extension if formerly PV but not currently, and at the leaves. Bench: 4759388

--- src/search.cpp
+++ src/search.cpp
@@ -1103,6 +1103,10 @@ moves_loop: // When in check, search starts from here
                && pos.non_pawn_material() <= 2 * RookValueMg)
           extension = 1;
 
+      // Former PV extension
+      else if (formerPv && depth < 2)
+          extension = 1;
+
       // Castling extension
       if (type_of(move) == CASTLING)
           extension = 1;
