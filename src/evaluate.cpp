86a7d7c71cbd5d04e18acad65a38ef3a775ee9ca
31m059 <37052095+31m059@users.noreply.github.com>
2020-04-19 04:24:36 -0400

    Captures in general. Bench: 4544181

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -629,7 +629,7 @@ namespace {
                 bonus -= make_score(0, king_proximity(Us, blockSq + Up) * w);
 
             // If the pawn is free to advance or capture, then increase the bonus
-            if (pos.empty(blockSq) || PawnAttacks[Us][s] & pos.pieces(Them, PAWN))
+            if (pos.empty(blockSq) || PawnAttacks[Us][s] & pos.pieces(Them))
             {
                 squaresToQueen = forward_file_bb(Us, s);
                 unsafeSquares = passed_pawn_span(Us, s);
