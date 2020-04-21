7345615dd0ef74e00b579a4675507b008f838ee6
31m059 <37052095+31m059@users.noreply.github.com>
2020-04-21 14:26:09 -0400

    Same, but SliderOnQueen instead. Bench: 4882357

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -560,12 +560,12 @@ namespace {
 
         b = attackedBy[Us][KNIGHT] & pos.attacks_from<KNIGHT>(s);
 
-        score += KnightOnQueen * (popcount(b & safe) + queenMob[Them] < 3);
+        score += KnightOnQueen * popcount(b & safe);
 
         b =  (attackedBy[Us][BISHOP] & pos.attacks_from<BISHOP>(s))
            | (attackedBy[Us][ROOK  ] & pos.attacks_from<ROOK  >(s));
 
-        score += SliderOnQueen * popcount(b & safe & attackedBy2[Us]);
+        score += SliderOnQueen * (popcount(b & safe & attackedBy2[Us]) + queenMob[Them] < 3);
     }
 
     if (T)
